02467010f97fded01a2c1191f354933ccceea727
dustnhgstrm <77700279+dustnhgstrm@users.noreply.github.com>
2021-08-03 15:45:27 -0400

    initial commit

---
+++ src/context/context.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+//can export as many contexts as I want.
+//we can only export default once
+export const TodoInputContext = React.createContext({});
+export const TodoContext = React.createContext({});
